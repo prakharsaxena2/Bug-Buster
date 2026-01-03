0be1bb9faae48b42a2b5832fcd035f3012f50730
Prakhar Saxena <Prakhar.Saxena2@ibm.com>
2026-01-03 22:59:31 +0530

    Add Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './styles.css';
+import { useReducer } from 'react';
+import TicketForm from './components/TicketForm';
+import ticketReducer from './reducers/ticketReducer';
+import TicketList from './components/TicketList';
+import { sortTickets } from './utilities/sortingUtilities';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const initialState = { tickets: [], editingTicket: null ,sortPreference:"High to Low"};
+
+    const [state, dispatch] = useReducer(ticketReducer, initialState);
+    const sortedTickets = sortTickets(state.tickets, state.sortPreference)
+    return (
+        <div className="App">
+            <div className='container'>
+                <h1>Bug Blaster</h1>
+                <TicketForm dispatch={dispatch}
+                 editingTicket={state.editingTicket}></TicketForm>
+                {state.tickets.length > 0 &&
+                    <div>
+                        <h3>All Tickets</h3>
+                    <select value={state.sortPreference} onChange={e=>dispatch({type:"SET_SORTING", payload:e.target.value})}>
+                        <option  value="High to Low">High to Low</option>
+                        <option value="Low to High">Low to High</option>
+                    </select>
+                        <TicketList tickets={sortedTickets} dispatch={dispatch}></TicketList>
+                    </div>
+                }
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
